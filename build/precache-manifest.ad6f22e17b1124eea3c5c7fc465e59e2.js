bd2757a58b663909913463657cf6ebbaa6549b48
Mithul <mithulmathi@gmail.com>
2019-05-01 14:18:03 -0500

    Build for production

---
+++ build/precache-manifest.ad6f22e17b1124eea3c5c7fc465e59e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3848c9927d93e575173",
+    "url": "/static/js/main.92103a26.chunk.js"
+  },
+  {
+    "revision": "8f8a755a94b0d6558687",
+    "url": "/static/js/2.2d585749.chunk.js"
+  },
+  {
+    "revision": "a3848c9927d93e575173",
+    "url": "/static/css/main.e2b39067.chunk.css"
+  },
+  {
+    "revision": "06a25012ac7e9760db33755c5a0d3ec2",
+    "url": "/index.html"
+  }
+];
